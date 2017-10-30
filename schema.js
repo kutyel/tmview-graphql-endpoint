781ec5a8bd6240c3b0a1306e3459573b0d8f3755
kutyel <flaviocorpa@gmail.com>
2017-10-30 11:35:42 +0100

    Refactor using graphql-tools 😍🤓

--- schema.js
+++ schema.js
@@ -1,60 +1,42 @@
 const fetch = require('node-fetch')
 const { promisify } = require('util')
 const { parseString } = require('xml2js')
-const parseXML = promisify(parseString)
-const {
-  GraphQLSchema,
-  GraphQLObjectType,
-  GraphQLString,
-  GraphQLList
-} = require('graphql')
+const { makeExecutableSchema } = require('graphql-tools')
 
-const TradeMarkType = new GraphQLObjectType({
-  name: 'TradeMark',
-  description: 'This is a Trade Mark registered on TMview',
-  fields: () => ({
-    name: {
-      type: GraphQLString,
-      description: 'Trade Mark name',
-      resolve: xml =>
-        xml.ApplicantDetails[0].Applicant[0].ApplicantAddressBook[0]
-          .FormattedNameAddress[0].Name[0].FreeFormatName[0]
-          .FreeFormatNameDetails[0].FreeFormatNameLine[0]
-    },
-    image: {
-      type: GraphQLString,
-      description: 'Trade Mark image (in case of having one)',
-      resolve: xml =>
-        xml.MarkImageDetails
-          ? xml.MarkImageDetails[0].MarkImage[0].MarkImageURI[0]
-          : 'NO IMAGE'
-    }
-  })
-})
+const typeDefs = `
+  type Query {
+    trademarks(name: String!): [TradeMark]
+  }
 
-module.exports = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'Query',
-    description: 'GraphQL query for TMview',
-    fields: () => ({
-      trademarks: {
-        type: new GraphQLList(TradeMarkType),
-        args: {
-          name: {
-            type: GraphQLString
-          }
-        },
-        resolve: (root, { name }) =>
-          fetch(`https://www.tmdn.org/tmview/trademark/search?tm=${name}`)
-            .then(res => res.text())
-            .then(parseXML)
-            .then(
-              xml =>
-                xml.Transaction.TradeMarkTransactionBody[0]
-                  .TransactionContentDetails[0].TransactionData[0]
-                  .TradeMarkDetails[0].TradeMark
-            )
-      }
-    })
-  })
-})
+  type TradeMark {
+    name: String!
+    image: String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    trademarks: (_, { name }) =>
+      fetch(`https://www.tmdn.org/tmview/trademark/search?tm=${name}`)
+        .then(res => res.text())
+        .then(promisify(parseString))
+        .then(
+          xml =>
+            xml.Transaction.TradeMarkTransactionBody[0]
+              .TransactionContentDetails[0].TransactionData[0]
+              .TradeMarkDetails[0].TradeMark
+        ),
+  },
+  TradeMark: {
+    name: xml =>
+      xml.ApplicantDetails[0].Applicant[0].ApplicantAddressBook[0]
+        .FormattedNameAddress[0].Name[0].FreeFormatName[0]
+        .FreeFormatNameDetails[0].FreeFormatNameLine[0],
+    image: xml =>
+      xml.MarkImageDetails
+        ? xml.MarkImageDetails[0].MarkImage[0].MarkImageURI[0]
+        : 'https://www.tmdn.org/tmview/trademark/image/placeholder.png',
+  },
+}
+
+module.exports = makeExecutableSchema({ typeDefs, resolvers })
